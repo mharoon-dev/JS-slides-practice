35f2aa106ff2fc151f54846a7f2f1dc7b12365b0
mharoon-dev <agar3d@gmail.com>
2023-09-28 15:39:15 +0500

    slide 1 to 11

---
+++ slide 1 to 8/app.js
@@ -0,0 +1,43 @@
+document.write("hello world")
+alert("hello people")
+console.log("hello everyone")
+
+var name = "jamal"                     // var variabel is the power of re-initialize and re-declear. 
+name = "rafiq"
+console.log(name + " ==> var variable")
+
+var full_name = "muhammad haroon"      // var with (_)
+console.log(full_name + " ==> variable with (_)")
+
+var name$ = "kamil"                   // var with $
+console.log(name$ + " ==> variable with dollar sign")
+
+var fullName = "sami"                 // var in camelCase
+console.log(fullName + " ==> variable in camelCase")
+
+var student1 = "samad"               // var in number
+console.log(fullName + " ==> variable with number")
+
+
+
+
+let children = "ali"                // let variable is the power of re-initialize but  doesn't re-declear.
+children = "ahmad"                  // re-declear the let variable.
+console.log(children +  " ==> let variable ")
+
+
+const child = "saqib"                // const variable doesn't re-initialize and re-declear.
+console.log(children +  " ==> const variable ")
+
+
+
+// task
+
+const firstName = "sami"
+const lastt_name = "ahmad"
+let address = "main street"
+address = "first street"
+
+console.log (firstName)
+console.log (lastt_name)
+console.log (address)
---
+++ slide 9 to 11/app.js
@@ -0,0 +1,22 @@
+// ("") ('')  quotation 
+// value in quotation is called string exampel: "haroon"
+// value with out quotation is called number exampel: 23
+// use plus (+) and comma (,) for the concatenation 
+
+const fName = "muhammad" 
+const lName = "haroon" 
+
+console.log(fName + ' ' + lName)
+
+let website  = "youtube"
+let url  = "https://www." + website + ".com"
+console.log(url)
+
+
+// task
+
+let street = "first street"
+let country = "pakistan"
+
+let fullMailingAddress = street + ' ' + country
+console.log(fullMailingAddress)
