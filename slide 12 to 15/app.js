03034c5122eaae57abf2b7f32d1dbdf58394964c
mharoon-dev <agar3d@gmail.com>
2023-10-05 19:35:28 +0500

    slide 12 to 15

---
+++ slide 12 to 15/app.js
@@ -0,0 +1,42 @@
+// // numbers 
+
+// let num1 = 89
+// let num2 = 79
+
+
+// console.log(num1 + num2) // add 
+// console.log(num1 - num2) // subtract
+// console.log(num1 * num2) // multiplication
+// console.log(num1 / num2) // division
+// console.log(num1 % num2) // modulas
+
+
+// // ranndom
+
+// let random1 = 4 + 6 + 10 * 10
+// let random2 = (4 + 6 + 10) * 10
+
+// console.log(random1)
+// console.log(random2)
+
+// // task
+
+// let score1 = 75
+// let score2 = 87
+// let score3 = 99
+
+// let calculation = score1 + score2 + score3
+// let average = calculation / 3
+// console.log(calculation + " ==> total" )
+// console.log(average +  " ==> average" )
+
+// // task 2
+
+
+
+// let plates = 28
+// let people = 7
+
+// let remaining =  plates - people
+// remaining++
+// console.log(remaining + " ==> remaining plates")
