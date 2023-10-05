30f4a670698df4d69443c10c86da5b824d675d17
mharoon-dev <agar3d@gmail.com>
2023-10-05 20:20:52 +0500

    slide 19 to 20

---
+++ slide 19 to 20/app.js
@@ -0,0 +1,10 @@
+// task
+
+let fruits = ["mango","apple","banana","strawberry", 85]
+
+let firstFruit = fruits[0]
+let lastFruit = fruits[fruits.length - 1] = firstFruit 
+
+console.log(fruits)
+console.log(firstFruit)
+console.log(lastFruit)
