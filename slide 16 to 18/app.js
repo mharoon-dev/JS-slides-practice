db986aed4cc65a190b463c3ebe52d72d6d8d87bf
mharoon-dev <agar3d@gmail.com>
2023-10-05 19:49:58 +0500

    slide 16 to 18

---
+++ slide 16 to 18/app.js
@@ -0,0 +1,23 @@
+// data types (7)
+// primitive
+// string 
+// number 
+// boolean
+// null
+// undefined
+// symbol
+
+
+const text = "some text"
+var number  = 1000
+let value1   = true
+const value2   = false
+var result   = null
+let name;
+
+console.log(typeof text + " ==> string")
+console.log(typeof number + " ==> number")
+console.log(typeof value1 + " ==> boolean")
+console.log(typeof value2 + " ==> boolean")
+console.log(typeof result + " ==> null")
+console.log(typeof name + " ==> undefined")
