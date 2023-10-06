5df7c0a76289b4b479ec59f38f29685b104811c8
mharoon-dev <agar3d@gmail.com>
2023-10-06 16:41:38 +0500

    slide 26 & 27

---
+++ slide 26 to 27/app.js
@@ -0,0 +1,44 @@
+// // objects
+
+
+// let obj = {
+//     name:"haroon",
+//     age:17,
+//     position:"student",
+//     religion:"islam"
+// }
+
+// console.log(obj)
+
+// console.log("name = " + obj.name) // haroon
+// console.log("age = " + obj.age) // 17
+// console.log("position = " + obj.position) // student
+// console.log("religion = " + obj.religion) // islam
+
+
+// // task
+
+// let car = {
+//     make: "honda",
+//     model: "civic",
+//     year: 2020,
+//     hybrid: true,
+//     colors: [
+//         "black",
+//         "white",
+//         "blue",
+//         "grey",
+//         "silver"
+//     ],
+//     drive:function drive() {
+//         return "drive a car."
+//     },
+//     stop:function stop() {
+//        return "stop a car."
+//     }
+// }
+
+// console.log(car.make)
+// console.log(car.colors[0])
+// console.log(car.drive())
+// console.log(car.stop())
