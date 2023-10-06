2ae3c04bb1bd7c68ce3092f2d20f1109ce84276f
mharoon-dev <agar3d@gmail.com>
2023-10-06 15:44:51 +0500

    slide 21 to 25

---
+++ slides 21 to 25/app.js
@@ -0,0 +1,45 @@
+// function myfun() {
+//     console.log("hello kamil")
+//     console.log("hello samad bhai")
+//     console.log("hello jamal bhai")
+//     console.log("hello sami")
+// }
+
+// console.log(myfun())
+
+// // function with arrguments and perameter 
+
+// function greet(name) {
+//     console.log("hello " + name)
+// }
+// console.log(greet("haroon")) // result = hello haroon
+
+
+// const wallHeight = 80;
+
+// function calculate(value) {
+//     return value * 2
+// } 
+
+// calculate(200) // 400
+// const width = calculate(100); // 200
+// const height = calculate(wallHeight) // 160
+
+// let diminsions = [width , height] // width = 200 height = 160
+// console.log("width = " +  diminsions[0])
+// console.log("height = " +  diminsions[1])
+
+
+
+// task 
+
+var order1 = 50
+var order2 = 70
+var order3 = 90
+
+function calculateTotal(subTotal , tax) {
+    calculateResult = subTotal + tax
+    return calculateResult
+}
+
+console.log(calculateTotal(order1, order3))
