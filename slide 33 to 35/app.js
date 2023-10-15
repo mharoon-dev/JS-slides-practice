3c9f3b41ad474ae6aa59438ffc5ba1f3c10b241d
mharoon-dev <haroon.aghani@gmail.com>
2023-10-15 15:23:43 +0500

    slide 33 to 35

---
+++ slide 33 to 35/app.js
@@ -0,0 +1,60 @@
+// switches
+
+// function rollTheDice() {
+//     let dice = Math.floor(Math.random() * 6) + 1
+
+//     switch (dice) {
+//         case 1:
+//             alert("you got 1")
+//             break;
+//         case 2:
+//             alert("you got 2")
+//             break;
+//         case 3:
+//             alert("you got 3")
+//             break;
+//         case 4:
+//             alert("you got 4")
+//             break;
+//         case 5:
+//             alert("you got 5")
+//             break;
+//         case 6:
+//             alert("you got 6")
+//             break;
+//         default:
+//             console.log("you did not roll the dice");
+    
+//     }
+//     return dice
+// }
+
+
+
+// task
+
+let person1 = {
+    name:"haroon",
+    age: 17,
+    status: "resident",
+}
+
+let person2 = {
+    name:"kamil",
+    age: 22,
+    status: "resident",
+}
+
+if (person1.age >= 18 && person1.age <= 25 && person1.status == "resident") {
+    console.log("perfect ==> person 1")
+} else {
+    console.log("sorry ==> person 1");
+}
+if (person2.age >= 18 && person2.age <= 25 && person2.status == "resident") {
+    console.log("perfect ==> person 2")
+} else {
+    console.log("sorry ==> person 2");
+}
+
+
+
