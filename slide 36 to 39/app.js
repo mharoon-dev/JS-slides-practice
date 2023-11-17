fcaac5292eb986be06bf9b20b6d78dec967818a1
mharoon-dev <haroon.aghani@gmail.com>
2023-11-17 14:14:49 +0500

    slide 36 to 39

---
+++ slide 36 to 39/app.js
@@ -0,0 +1,17 @@
+let amount = 5
+
+// while (amount <= 20) {
+//     console.log(amount);
+//     ++amount
+// }
+
+
+// do {
+//     console.log(amount);
+//     amount++
+// } while (amount > 10)
+
+// for (i = 0 ; i < amount ; i++) {
+//     console.log("hello i am for loop");
+// }
+
