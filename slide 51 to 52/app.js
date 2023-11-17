39590325a0c130ef8fe9a4a70ec86dacc4510289
mharoon-dev <haroon.aghani@gmail.com>
2023-11-17 14:52:44 +0500

    slide 51 to 52

---
+++ slide 51 to 52/app.js
@@ -0,0 +1,21 @@
+// const name = "haroon"
+// const age = 17
+// const sentence = "my name is " + name +  " and i am " + age + "year old" 
+
+// // templates  literals
+// // backtick characters `` - above tab (left from one)
+// // interpolation ${} - insert experssion (value)
+
+
+// let sentenceWithBackticks = `my name is ${name} and i am ${age} years old`
+
+// task 
+
+// function fulllName(fName,lName) {
+//     let fullName = `${fName} ${lName}`
+//     console.log(fullName.toUpperCase());
+// }
+
+// fulllName("muhammad",'haroon')
+// fulllName("sami",'ahmad')
+// fulllName("kamil",'raza')
