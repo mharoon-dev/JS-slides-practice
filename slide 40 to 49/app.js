d5a571b2adc6f25234ef9fca2d7fe54b4a14af66
mharoon-dev <haroon.aghani@gmail.com>
2023-11-17 14:32:28 +0500

    slide 40 to 49

---
+++ slide 40 to 49/app.js
@@ -0,0 +1,15 @@
+let person  = {
+    name: "haroon",
+    grade:10,
+    age: 17
+}
+
+console.log(person.name);
+console.log(person.name.length + " ==> length of name");
+console.log(`${person.name.charAt(5)} ==> charAt 5`);
+console.log(person.name.indexOf("n"));
+console.log(person.name.toLowerCase());
+console.log(person.name.toUpperCase());
+console.log(person.name.includes("aroon"));
+console.log(person.name.startsWith("ali"));
+console.log(person.name.slice(0 , 3));
